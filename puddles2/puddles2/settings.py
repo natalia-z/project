05a31ec6931b049affbb6d6cc11dc403765a9aa2
natalia-z <nataliazdeb@gmail.com>
2017-04-22 22:21:19 +0100

    Settings

--- puddles2/puddles2/settings.py
+++ puddles2/puddles2/settings.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'bonjourdata@gmail.com'
@@ -124,10 +124,11 @@ DATE_FORMAT = "Y-m-d"
 
 STATIC_URL = '/static/'
 
+
 if DEBUG:
     MEDIA_URL = '/media/'
-    STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static", "static-only")
-    MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static", "media")
+    STATIC_ROOT = os.path.join(os.path.dirname(os.path.dirname(BASE_DIR)), "static", "static-only")
+    MEDIA_ROOT = os.path.join(os.path.dirname(os.path.dirname(BASE_DIR)), "static", "media")
     STATICFILES_DIRS = (
-        os.path.join(os.path.dirname(BASE_DIR,), "static", "static"),
+        os.path.join(os.path.dirname(os.path.dirname(BASE_DIR)), "static", "static"),
     )
