26a64fb917736b520528bb05e4f1edcc7bf306e0
natalia-z <nataliazdeb@gmail.com>
2017-05-06 16:55:06 +0100

    ''

--- puddles2/puddles2/settings.py
+++ puddles2/puddles2/settings.py
@@ -11,6 +11,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'bonjourdata@gmail.com'
@@ -126,6 +127,7 @@ DATE_FORMAT = "Y-m-d"
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(os.path.dirname(os.path.dirname(BASE_DIR)), "static", "static")
 
 if DEBUG:
     MEDIA_URL = '/media/'
