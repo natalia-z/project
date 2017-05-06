1d6cec81c95c67e233d9c63a53ac392291cdc7f9
natalia-z <nataliazdeb@gmail.com>
2017-05-06 16:39:02 +0100

    ''

--- puddles2/puddles2/settings.py
+++ puddles2/puddles2/settings.py
@@ -16,6 +16,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'bonjourdata@gmail.com'
 EMAIL_HOST_PASSWORD = 'yuvygrsasifrcwfc'
 EMAIL_PORT = '587'
+ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_USE_TLS = True
 
 # Quick-start development settings - unsuitable for production
