2276d6eb947600d02fa26120e0012427ee889056
natalia-z <nataliazdeb@gmail.com>
2017-05-06 16:51:43 +0100

    ''

--- puddles2/puddles2/wsgi.py
+++ puddles2/puddles2/wsgi.py
@@ -10,7 +10,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "puddles.settings")
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "puddles.settings")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
