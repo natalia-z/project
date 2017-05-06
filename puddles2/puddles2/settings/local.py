2667b4fc456fa61bd8eb1931fce56abd7246a0e8
natalia-z <nataliazdeb@gmail.com>
2017-05-06 16:35:24 +0100

    settings

--- puddles2/puddles2/settings/local.py
+++ puddles2/puddles2/settings/local.py
@@ -14,7 +14,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'bonjourdata@gmail.com'
-EMAIL_HOST_PASSWORD = 'entourage123'
+EMAIL_HOST_PASSWORD = 'yuvygrsasifrcwfc'
+ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_PORT = '587'
 EMAIL_USE_TLS = True
 
