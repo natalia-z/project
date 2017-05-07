fec9ae0820eb41328b90c6ea4f8a145d5096f74b
natalia-z <nataliazdeb@gmail.com>
2017-05-07 01:09:50 +0100

    ''

--- puddles2/puddlesbooking/views.py
+++ puddles2/puddlesbooking/views.py
@@ -2,7 +2,7 @@
 from datetime import datetime, timedelta
 import datetime
 import operator
-import requests
+#import requests
 from django.shortcuts import render, redirect
 from django.core.mail import send_mail
 from django.conf import settings
